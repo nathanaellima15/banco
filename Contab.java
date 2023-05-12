6ff30b7d0f5e7a9645ba89534f317e9845be5db9
Nathanael Lima <128553439+nathanaellima15@users.noreply.github.com>
2023-05-12 20:45:37 -0300

    Add files via upload
    arquivos do programa conta de banco

---
+++ Contab.java
@@ -0,0 +1,27 @@
+public class Contab {
+
+    private int Numero;
+    private String NomeAgencia;
+    private String Banco;
+    private String TipoConta;
+    private float Saldo;
+
+    public void abrir( Numero, NomeAgencia, Banco, TipoConta, Saldo) {
+    
+    }
+    public void encerrar() {
+    
+    }
+    public void consultar() {
+    
+    }
+    public void saldo() {
+    
+    }
+    public void creditar() {
+    
+    }
+    public void debitar() {
+    
+    }
+}
---
+++ Contabanco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contabanco;
+
+/**
+ *
+ * @author aluno
+ */
+public class Contabanco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
